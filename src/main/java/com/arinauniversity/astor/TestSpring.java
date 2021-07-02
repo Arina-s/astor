b06e173d5849b0ddd814d44e5841dd840dd5021b
Arina Slizh <arina_slizh@epam.com>
2021-07-02 21:41:28 +0300

    Fixed TestSpring.java
    Added applicationContext.close()

--- src/main/java/com/arinauniversity/astor/TestSpring.java
+++ src/main/java/com/arinauniversity/astor/TestSpring.java
@@ -9,6 +9,7 @@ public class TestSpring {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         TestBean testBean = applicationContext.getBean("testBean", TestBean.class);
         System.out.println(testBean);
+        applicationContext.close();
     }
 
 }
