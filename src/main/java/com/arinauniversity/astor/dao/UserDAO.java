46809986fd2157ebbde4e21464ff955bad7df1d1
Arina Slizh <arina_slizh@epam.com>
2021-07-07 10:33:59 +0300

    Lesson 25 alishev - Added postgresql - Implement getAllUsers with DB - Implement saveUser with DB

--- src/main/java/com/arinauniversity/astor/dao/UserDAO.java
+++ src/main/java/com/arinauniversity/astor/dao/UserDAO.java
@@ -3,23 +3,52 @@ package com.arinauniversity.astor.dao;
 import com.arinauniversity.astor.model.User;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserDAO {
 
-    private static int count;
-    private final List<User> userList;
+    private static final String URL = "jdbc:postgresql://localhost:5433/astor";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123";
+    private static int count = 5;
+    private List<User> userList;
+    private Connection connection;
 
-    {
-        userList = new ArrayList<>();
-        userList.add(new User(++count, "Mary", 24, "mary78@mail.ru"));
-        userList.add(new User(++count, "Nikolas", 34, "nikolaTesla2_2@gmail.com"));
-        userList.add(new User(++count, "Simon", 18, "mario_rty@mail.ru"));
+    public UserDAO() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<User> getUsers() {
+        userList = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setAge(resultSet.getInt("age"));
+                user.setEmail(resultSet.getString("email"));
+                userList.add(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return userList;
     }
 
@@ -31,8 +60,15 @@ public class UserDAO {
     }
 
     public void save(User user) {
-        user.setId(++count);
-        userList.add(user);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = String.format("INSERT INTO users VALUES(%s, '%s', %s, '%s')", ++count, user.getName(),
+                    user.getAge(), user.getEmail());
+            statement.executeQuery(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 
     public void update(int id, User updatedUser) {
